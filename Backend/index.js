f8647efca2d69b78d759a6c6bec1522191299e6f
Hamza11067 <hamzakhalid1067@gmail.com>
2024-03-28 22:40:11 +0500

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+// console.log("hi");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! from Hamza')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
