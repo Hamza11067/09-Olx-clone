c41eb84498bc3eb0aef69c8bf4e544d35ca5c5f3
Hamza11067 <hamzakhalid1067@gmail.com>
2024-03-29 21:19:26 +0500

    some changes

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div id="footer">
+      <div id="sub-footer">
+        <div>
+          <img src="/public/assets/olx-footer" alt="app-img" />
+        </div>
+        <div>
+          <h1>TRY THE OLX APP</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
