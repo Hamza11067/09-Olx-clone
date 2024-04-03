f0d20d5da4455b366d90eb8c618eead5161f6af7
Hamza11067 <hamzakhalid1067@gmail.com>
2024-04-03 22:29:16 +0500

    connecting mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/olx");
 
-app.get('/', (req, res) => {
-  res.send('Hello World! from Hamza')
-})
+const Users = mongoose.model("Users", { username: String, password: String });
+
+app.get("/", (req, res) => {
+  res.send("Hello World! from Hamza");
+});
+
+app.get("/signup", (req, res) => {
+  // const username = req.body.username;
+  // const password = req.body.password;
+  const user = new Users({ username: "Hamza", password: "Hamza1067" });
+  user.save().then(() => console.log("Save ho gaya"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
