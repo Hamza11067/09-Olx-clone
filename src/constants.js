d539509956e1fefc7ed9627e8007a6c212e55890
Hamza11067 <hamzakhalid1067@gmail.com>
2024-05-09 12:00:18 +0500

    just trying to connect it to backend

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000' || 'https://09-olx-clone-5zjhnqgvs-hamzas-projects-f3fcc6ec.vercel.app';
+const API_URL = 'http://localhost:3000' || 'https://09-olx-clone-backend.vercel.app';
 
 
 export default API_URL;
